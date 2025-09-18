a06858d1482a9261f7caee1ace492846cd9e5d83
Olajide <oolajide91@gmail.com>
2025-09-18 13:23:50 +0100

    updates to settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -58,6 +58,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.permissions.IsAuthenticated'
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    
+}
+
 ROOT_URLCONF = 'messaging_app.urls'
 
 TEMPLATES = [
