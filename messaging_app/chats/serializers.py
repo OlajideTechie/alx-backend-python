87b26f6011afd6ce68fd55d04fd301728b79ec21
Olajide <oolajide91@gmail.com>
2025-09-20 11:35:35 +0100

    Create serializers to define the many to many relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from .models import CustomUser, Conversation, Message
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError as DjangoValidationError
 
 
 # ------------------------
 # USER SERIALIZER
 # ------------------------
 class UserSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
     class Meta:
         model = CustomUser
         fields = [
@@ -15,17 +19,39 @@ class UserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "phone_number",
+            "password",
             "role",
             "created_at",
         ]
+        extra_kwargs = {
+            'password': {'required': True},
+            "first_name": {"required": True},
+            "last_name": {"required": True},
+            "email": {"required": True},
+            "username": {"required": True},
+            "phone_number": {"required": False},
+        }
+        
+    
+    def validate_email(self, value):
+        """Ensure the email is valid and unique."""
+        value = value.strip().lower()
+        try:
+            validate_email(value)
+        except DjangoValidationError:
+            raise serializers.ValidationError("Enter a valid email address.")
+
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
 
 
 # ------------------------
 # MESSAGE SERIALIZER
 # ------------------------
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserSerializer(source="sender_id", read_only=True)     # nested sender
-    recipient = UserSerializer(read_only=True)                      # nested recipient
+    sender = UserSerializer(source="sender_id", read_only=True)
+    recipient = UserSerializer(read_only=True)                      
 
     class Meta:
         model = Message
