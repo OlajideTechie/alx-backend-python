104d199c99243b0c62f190a358d258318fa782e4
Olajide <oolajide91@gmail.com>
2025-10-05 18:03:55 +0100

    checker issue

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -61,7 +61,14 @@ def send_message(request, receiver_id):
 
 @login_required
 def unread_messages_view(request):
-    unread_messages = Message.unread.for_user(request.user)
+    """
+    Display unread messages for the logged-in user.
+    """
+    unread_messages = (
+        Message.unread.unread_for_user(request.user)
+        .only("id", "sender", "content", "timestamp")
+    )
+
     return render(request, "messaging/unread_messages.html", {
         "unread_messages": unread_messages
     })
